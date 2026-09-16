98e9ee64ad467dc5f85ede4437c3334b49c4017c
gabrielcsilveira-code <gabriel.csilveira@aluno.impacta.edu.br>
2026-09-16 01:58:23 +0000

    Adiciona atividade 1 de Python

---
+++ AP1 DE PYTHON/ATIVIDADE1.py
@@ -0,0 +1,13 @@
+nome = "Harry Potter"
+mensagem = f"Olá, {nome}!"
+print(mensagem)
+
+print("Vamos fazer algumas contas:")
+
+conta = 2 + 3 * 4
+soma = 10 + 5
+
+print(f"2 + 3 * 4 = {conta}")
+print(f"10 + 5 = {soma}")
+
+print("Fim da sessão!")
